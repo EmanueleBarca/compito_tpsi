2e56911a2f63e69da59e90db482fc66279a20df6
EmanueleBarca <emanuele.barca@davincimilazzo.edu.it>
2024-12-02 09:00:49 +0100

    consegna compito tpsi Emanuele Barca

---
+++ emanuele barca compito tpsi.cpp
@@ -0,0 +1,18 @@
+math.c
+#include <stdio.h>
+#include hello.c
+int main(void){
+
+
+    int num1, num2;
+    int risultato;
+    char operazione;
+ {
+    printf("inserisci il primo numero");
+    scanf("num1");
+    printf("inserisci il secondo numero");
+    scanf("num2");
+ }
+}
+
+return 0
